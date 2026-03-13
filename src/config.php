149ca2277abaec83a70879cc359f57b6fe1bb689
Kevin <contact@kevinpaulidor.de>
2026-03-13 01:31:20 +0100

    chore: move database config to dotenv

--- src/config.php
+++ src/config.php
@@ -1,13 +1,22 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 // Basic connection settings
-$databaseHost = '100.95.147.14';
-$databaseUsername = 'baki';
-$databasePassword = 'Nasa1994';
+$databaseHost = '<host_address>';
+$databaseUsername = '<user_name>';
+$databasePassword = '******';
 $databaseName = 'rolodex';
 
 // Connect to the database
-$conn = new mysqli($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-
+$conn = new mysqli(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    $_ENV['DB_NAME'],
+    (int) $_ENV['DB_PORT']
+);
 if($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }

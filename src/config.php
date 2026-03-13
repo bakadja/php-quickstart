2ea99077fe5121c80f81004793369fe10a034a6e
Kevin Ngongang <70435295+bakadja@users.noreply.github.com>
2026-03-13 10:56:11 +0100

    fix: harden env config defaults and redirect exit

--- src/config.php
+++ src/config.php
@@ -3,21 +3,16 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
-// Basic connection settings
-$databaseHost = '<host_address>';
-$databaseUsername = '<user_name>';
-$databasePassword = '******';
-$databaseName = 'rolodex';
 
 // Connect to the database
 $conn = new mysqli(
-    $_ENV['DB_HOST'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASS'],
-    $_ENV['DB_NAME'],
-    (int) $_ENV['DB_PORT']
+    $_ENV['DB_HOST'] ?? '127.0.0.1',
+    $_ENV['DB_USER'] ?? '',
+    $_ENV['DB_PASS'] ?? '',
+    $_ENV['DB_NAME'] ?? 'rolodex',
+    (int) ($_ENV['DB_PORT'] ?? 3306)
 );
-if($conn->connect_error) {
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
